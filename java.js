f22165c6bb08e0a560d6c197af37751548e46451
André Bragança <andre08ramos@gmail.com>
2025-10-21 08:48:35 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+function DarkMode(){
+    document.body.classList.toggle("dark_mode")
+}
+
+
+modal = document.getElementById("modal1")
+
+function AbrirModal(){
+    console.log("asda")
+    modal.style.display = "block"
+}
+
+function FecharModal(){
+    console.log("da")
+    modal.style.display = "none"
+}
+
+
+
+
